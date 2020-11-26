0b536406909e4dd3988024c5a7499f4950544bff
raul.garcia.f <raul.garcia.f@ricoh.es>
2020-11-26 13:22:59 +0100

    Create Request example

---
+++ src/main/java/es/ricoh/todolist/controller/dto/Request.java
@@ -0,0 +1,13 @@
+package es.ricoh.todolist.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@Getter@Setter
+public class Request {
+
+}
--- src/test/java/es/ricoh/todolist/ApplicationTest.java
+++ src/test/java/es/ricoh/todolist/ApplicationTest.java
@@ -3,6 +3,8 @@ package es.ricoh.todolist;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import es.ricoh.todolist.controller.dto.Request;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -11,6 +13,7 @@ public class ApplicationTest {
 
     @Test
     public void testHelloEndpoint() {
+    	
         given()
           .when().get("/isAlive")
           .then()
